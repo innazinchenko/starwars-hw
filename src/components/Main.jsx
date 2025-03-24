ac0f408d8359a6f4ca3e0ce9e0df54113d9c70a0
innazinchenko <spangle_red@inbox.ru>
2025-03-24 21:07:26 +0100

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import Pictures from "./Pictures.jsx";
+import Text from "./Text.jsx";
+import Skywalker from "./Skywalker.jsx";
+
+const Main = () => {
+    return (
+        <main className="clearfix">
+            <Skywalker/>
+            <Pictures/>
+            <Text/>
+        </main>
+    );
+};
+
+export default Main;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+const Navigation = () => {
+    return (
+        <nav className="fixed-top mt-2 ms-5">
+            <ul className="nav">
+                <li className="nav-item btn btn-danger mx-1">Home</li>
+                <li className="nav-item btn btn-danger mx-1">About me</li>
+                <li className="nav-item btn btn-danger mx-1">Star Wars</li>
+                <li className="nav-item btn btn-danger mx-1">Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
